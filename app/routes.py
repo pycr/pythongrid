4bc298118da09faa0693798ad7d6e19890247311
chenster <chenster@gmail.com>
2020-04-27 07:32:33 -0700

    Repalce tab with 4 spaces per PEP-8
    Thanks, Reddit!

--- app/routes.py
+++ app/routes.py
@@ -3,27 +3,27 @@ from app import app
 from app.grid import PythonGrid
 from app.data import PythonGridDbData
 
-@app.route('/')	
+@app.route('/')    
 def index():
-	grid = PythonGrid('SELECT * FROM orders', 'orderNumber', 'orders')
+    grid = PythonGrid('SELECT * FROM orders', 'orderNumber', 'orders')
 
-	grid.set_caption('Orders Table')
-	grid.set_col_title('orderDate', 'Order Date')
-	grid.set_col_title('orderNumber', 'Order #')
-	grid.set_col_hidden(['customerNumber, logTime, shippedDate, requiredDate'])
-	grid.set_pagesize(20)
-	grid.set_dimension(800, 400)
-	grid.enable_search(True)
-	grid.enable_rownumbers(True)
-	grid.enable_pagecount(True)
-	grid.set_col_align('status', 'center')
-	grid.set_col_width('comments', 600)
+    grid.set_caption('Orders Table')
+    grid.set_col_title('orderDate', 'Order Date')
+    grid.set_col_title('orderNumber', 'Order #')
+    grid.set_col_hidden(['customerNumber, logTime, shippedDate, requiredDate'])
+    grid.set_pagesize(20)
+    grid.set_dimension(800, 400)
+    grid.enable_search(True)
+    grid.enable_rownumbers(True)
+    grid.enable_pagecount(True)
+    grid.set_col_align('status', 'center')
+    grid.set_col_width('comments', 600)
 
-	return render_template('grid.html', title='demo', grid=grid)
+    return render_template('grid.html', title='demo', grid=grid)
 
 @app.route('/data', methods=['GET', 'POST'])
 def data():
-	data = PythonGridDbData('SELECT * FROM orders')
-	return data.getData()
+    data = PythonGridDbData('SELECT * FROM orders')
+    return data.getData()
 
 
--- config.py
+++ config.py
@@ -3,24 +3,24 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 
-	ABS_PATH = '/'
+    ABS_PATH = '/'
 
-	# postgres
-	PYTHONGRID_DB_HOSTNAME = 'localhost'
-	PYTHONGRID_DB_NAME = 'sampledb'
-	PYTHONGRID_DB_USERNAME = 'root'
-	PYTHONGRID_DB_PASSWORD = 'root'
-	PYTHONGRID_DB_TYPE = 'postgres+psycopg2'
-	PYTHONGRID_DB_SOCKET = ''
-	PYTHONGRID_DB_CHARSET = 'utf-8'
+    # postgres
+    PYTHONGRID_DB_HOSTNAME = 'localhost'
+    PYTHONGRID_DB_NAME = 'sampledb'
+    PYTHONGRID_DB_USERNAME = 'root'
+    PYTHONGRID_DB_PASSWORD = 'root'
+    PYTHONGRID_DB_TYPE = 'postgres+psycopg2'
+    PYTHONGRID_DB_SOCKET = ''
+    PYTHONGRID_DB_CHARSET = 'utf-8'
 
-	# mysql
-	""" 
-	PYTHONGRID_DB_HOSTNAME = ''
-	PYTHONGRID_DB_NAME = 'sampledb'
-	PYTHONGRID_DB_USERNAME = 'root'
-	PYTHONGRID_DB_PASSWORD = 'root'
-	PYTHONGRID_DB_TYPE = 'mysql+pymysql'
-	PYTHONGRID_DB_SOCKET = '/Applications/MAMP/tmp/mysql/mysql.sock'
-	PYTHONGRID_DB_CHARSET = 'utf-8'
-	"""
+    # mysql
+    """ 
+    PYTHONGRID_DB_HOSTNAME = ''
+    PYTHONGRID_DB_NAME = 'sampledb'
+    PYTHONGRID_DB_USERNAME = 'root'
+    PYTHONGRID_DB_PASSWORD = 'root'
+    PYTHONGRID_DB_TYPE = 'mysql+pymysql'
+    PYTHONGRID_DB_SOCKET = '/Applications/MAMP/tmp/mysql/mysql.sock'
+    PYTHONGRID_DB_CHARSET = 'utf-8'
+    """
