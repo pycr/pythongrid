4963f0faee13803405a3193b7a94587442cf4a7c
PHPControls <richard@phpcontrols.com>
2023-08-21 19:22:27 -0700

    Use flask_session extension for server-side session

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
+from flask_session import Session
 import logging
 from config import Config
-# from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
+# start Flask session
+app.config['SECRET_KEY'] = "<REPLACE WITH YOUR OWN SECRET KEY HERE>"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 app.debug = True
 app.config.from_object(Config)
-# toolbar = DebugToolbarExtension(app)
 
 # logging in terminal display (no log file)
 logging.basicConfig(level=logging.DEBUG)
