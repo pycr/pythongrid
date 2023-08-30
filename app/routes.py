2771c44809d3817b8df607570d08dd2ab2ce536e
PHPControls <richard@phpcontrols.com>
2023-08-30 13:32:26 -0700

    Select multiple rows

--- app/routes.py
+++ app/routes.py
@@ -20,6 +20,7 @@ def index():
     grid.set_col_align('status', 'center')
     grid.set_col_width('comments', 600)
     grid.enable_export()
+    grid.enable_edit('FORM')
 
     return render_template('grid.html', title='demo', grid=grid)
 
